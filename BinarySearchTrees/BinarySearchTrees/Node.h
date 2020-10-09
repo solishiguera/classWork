dd6993ac0859a44e2c08dff0ff5258fbcaaeb98c
solishiguera <a00827847@itesm.mx>
2020-10-09 16:25:33 -0500

    added Binary search tree

---
+++ BinarySearchTrees/BinarySearchTrees/Node.h
@@ -0,0 +1,32 @@
+//
+//  Nodo.h
+//  node
+//
+//  Created by Diego Solis on 9/21/20.
+//
+
+#pragma once
+
+
+template<class T>
+struct Node {
+    T data;
+    Node<T>* left;
+    Node<T>* right;
+    Node(T data);
+    Node(T data, Node<T>* left, Node<T>* right);
+};
+
+template <class T>
+Node<T>::Node(T data) {
+    this->data = data;
+    this->left = NULL;
+    this->right = NULL;
+}
+
+template <class T>
+Node<T>::Node(T data, Node<T>* next) {
+    this->data = data;
+    this->left = left;
+    this->right = right;
+}
---
+++ BinarySearchTrees/BinarySearchTrees/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  BinarySearchTrees
+//
+//  Created by Diego Solis on 10/9/20.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
