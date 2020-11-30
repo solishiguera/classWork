e26b765ded5b287cef076957581f4e8626203825
Diego <A00827847@itesm.mx>
2020-11-30 11:42:42 -0600

    Actividad hashing

---
+++ Hashing/Hashing/HashQ.h
@@ -0,0 +1,12 @@
+//
+//  HashQ.h
+//  Hashing
+//
+//  Created by Diego Solis on 11/30/20.
+//
+
+#ifndef HashQ_h
+#define HashQ_h
+
+
+#endif /* HashQ_h */
---
+++ Hashing/Hashing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hashing
+//
+//  Created by Diego Solis on 11/30/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
